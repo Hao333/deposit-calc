3061c833d516ccd37ca19896904f51d52acaab7a
Кустов Егор Евгеньевич <ip611s13@awp04.csc.local>
2017-02-22 11:54:16 +0700

    programma

--- lab.c
+++ lab.c
@@ -1,6 +1,7 @@
 int main(){
 int a,b;
 int a,b;
+int a,b;
 printf ("vvedite summu ne menee 1k;a=");
 scanf("%d",&a);
 printf ("\n");
@@ -14,6 +15,17 @@ printf ("\n");
 	if (b<0 or b>365) {
 	printf("error ne vernoe kolichestvo dney");
 	return 0;
-}	
+}
+	if (b>=0 and b<=30)
+		a=a*0.9;
+	if (b>=31 and b<=120)
+		a=a*1.02;
+	if (b>=121 and b<=240)
+		a=a*1.06;
+	if (b>=241 and b<=365)
+		a=a*1.12;
+printf("summa v konce sroka=%d",a);
+printf ("\n");
+system("pause");
     return 0
 }
