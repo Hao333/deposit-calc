abbb95c2cb553cf04f80fc74dfe3c771327d026c
Кустов Егор Евгеньевич <ip611s13@awp04.csc.local>
2017-02-22 11:52:49 +0700

    vvod chisel

--- lab.c
+++ lab.c
@@ -1,4 +1,10 @@
 int main(){
-
+int a,b;
+printf ("vvedite summu ne menee 1k;a=");
+scanf("%d",&a);
+printf ("\n");
+printf ("vvedite srok vklada ot 0 do 365;b=");
+scanf("%d",&b);
+printf ("\n");	
     return 0
 }
