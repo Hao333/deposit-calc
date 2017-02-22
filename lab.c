eac44b6099932b081e4cbc138583d8f3fe650ee3
Кустов Егор Евгеньевич <ip611s13@awp04.csc.local>
2017-02-22 11:53:48 +0700

    proverka yslovia

--- lab.c
+++ lab.c
@@ -1,10 +1,19 @@
 int main(){
 int a,b;
+int a,b;
 printf ("vvedite summu ne menee 1k;a=");
 scanf("%d",&a);
 printf ("\n");
+	if (a<1000) {
+	printf ("error summa menee 1k");
+	return 0;
+}
 printf ("vvedite srok vklada ot 0 do 365;b=");
 scanf("%d",&b);
-printf ("\n");	
+printf ("\n");
+	if (b<0 or b>365) {
+	printf("error ne vernoe kolichestvo dney");
+	return 0;
+}	
     return 0
 }
