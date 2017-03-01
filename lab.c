b98f5d125bb617b0684f1d2f31531fdf2c8d5c69
Кустов Егор Евгеньевич <bloody.shadow98@gmail.com>
2017-03-01 12:56:13 +0700

    finally version v2

--- lab.c
+++ lab.c
@@ -7,20 +7,22 @@ int proverkaA(int a){
     }
 int proverkaB(int b){
     if (b<0 || b>365)
-    return 0;
+    return -1;
     else return b;
-    
     }
 double summa(int a, int b){
+    double c;
     if (b>=0 && b<=30)
-	a=a*0.9;
+	c=a*0.9;
     if (b>=31 && b<=120)
-	a=a*1.02;
+	c=a*1.02;
     if (b>=121 && b<=240)
-	a=a*1.06;
+	c=a*1.06;
     if (b>=241 && b<=365)
-	a=a*1.12;
-    return a;
+	c=a*1.12;
+    if (c==a)
+	c=0;
+    return c;
     }
 
 int main(){
