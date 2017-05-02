b579f1fa47f27edf7edc63c277edaf50c9e837b4
Hao333 <bloody.shadow98@gmail.com>
2017-05-02 19:12:43 +0700

    Update validation_test.c

--- test/validation_test.c
+++ test/validation_test.c
@@ -6,19 +6,19 @@ CTEST(validation, test1) {
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite1, test2) {
+CTEST(validation, test2) {
     int result = chooseSrok(157);
     int expected = 3;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite1, test3) {
+CTEST(validation, test3) {
     int result = chooseSrok(25);
     int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite1, test4) {
+CTEST(validation, test4) {
     int result = chooseSrok(69);
     int expected = 2;
     ASSERT_EQUAL(expected, result);
