0cf8e305cc4eaf4a0c4faafaf354f3c7c459b0ee
Hao333 <bloody.shadow98@gmail.com>
2017-05-02 19:13:35 +0700

    Update deposit_test.c

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -1,24 +1,24 @@
 #include "../thirdparty/ctest.h"
 
-CTEST(suite2, test1) {
+CTEST(deposit3, test1) {
     float result = calc(5000, 3);
     float expected = 5300;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite2, test2) {
+CTEST(deposit4, test2) {
     float result = calc(1790, 4);
     float expected = 2004.8;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite2, test3) {
+CTEST(deposit1, test3) {
     float result = calc(6130, 1);
     float expected = 5517;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(suite2, test4) {
+CTEST(deposit2, test4) {
     float result = calc(2684, 2);
     float expected = 2737.68;
     ASSERT_EQUAL(expected, result);
