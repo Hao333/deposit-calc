0f71726ae95d6107aeef07a360d19174b3340d76
Hao333 <bloody.shadow98@gmail.com>
2017-05-02 19:13:52 +0700

    Update validation_test.c

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,24 +1,24 @@
 #include "../thirdparty/ctest.h"
 
-CTEST(validation, test1) {
+CTEST(validation4, test1) {
     int result = chooseSrok(241);
     int expected = 4;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(validation, test2) {
+CTEST(validation3, test2) {
     int result = chooseSrok(157);
     int expected = 3;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(validation, test3) {
+CTEST(validation1, test3) {
     int result = chooseSrok(25);
     int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(validation, test4) {
+CTEST(validation2, test4) {
     int result = chooseSrok(69);
     int expected = 2;
     ASSERT_EQUAL(expected, result);
